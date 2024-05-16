52bd41b2696e76cf0836a3bd42802f19c057c461
M1L70N <dangerd660@gmail.com>
2024-05-16 01:08:30 -0400

    123

--- LIMA.lua
+++ LIMA.lua
@@ -1,7 +1,6 @@
 local ips = {
     "1.1.1.1", -- cloud flare
     "45.173.164.92", -- meu proprio IP
-    "191.96.81.104"  -- alexandre_bay_ - jayjay - <@986004494594764910>
 }
 
 local auth = false
